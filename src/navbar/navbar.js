80f3c1d3885a3d1e14196374bead07bebdad40c8
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-11-27 21:53:00 +0200

    Add Navbar Component and represent it in the App

---
+++ src/navbar/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Navbar extends React.Component{
+
+    constructor()
+    {
+        super()
+    }
+
+    render()
+    {
+        return (<h1>This is navbar</h1>);
+    }
+
+}
